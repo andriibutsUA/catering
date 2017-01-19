14825f2d98fea58a6dd7a4024083f6e3284dfd01
andriibutsUA <andriibuts@gmail.com>
2017-01-19 07:56:36 +0200

    buyer application for catering project

---
+++ buyer/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+from django.utils import timezone
+from django.utils.encoding import smart_unicode
+
+
+class Ingredient(models.Model):
+    title = models.CharField(max_length=200)
+    UNIT = (
+        ('g', 'Грамм'),
+        ('l', 'Литр'),
+        ('q', 'Штуки'),
+    )
+
+    def __str__(self):
+        return self.title

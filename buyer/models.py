363fee8779048fe5a08f29ddf38bf40259a0e4ec
andriibutsUA <andriibuts@gmail.com>
2017-01-19 08:39:36 +0200

    i should use venv

--- buyer/models.py
+++ buyer/models.py
@@ -4,12 +4,17 @@ from django.utils import timezone
 
 class Ingredient(models.Model):
     title = models.CharField(max_length=200)
-    UNIT = (
+    unit = models.
+    UNIT_CHOICES = (
         ('g', 'Грамм'),
         ('l', 'Литр'),
         ('q', 'Штуки'),
     )
     supplier = models.CharField(max_length=200)
+    unit = models.CharField(
+        max_length=1,
+        choices=UNIT_CHOICES,
+    )
 
     def __str__(self):
         return self.title

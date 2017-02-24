0fe9b3ffb0436ca59e62b6d20bb1cf94fea9800c
andriibutsUA <andriibuts@gmail.com>
2017-02-24 14:34:08 +0200

    experimenting with views

--- buyer/views.py
+++ buyer/views.py
@@ -8,7 +8,3 @@ def index(request):
 
 def dish(request, dish_id):
     return HttpResponse("You're looking at dish %s" % dish_id)
-
-
-def dish(request, ingredient_id):
-    return HttpResponse("You're looking at ingredient %s" % ingredient_id)
